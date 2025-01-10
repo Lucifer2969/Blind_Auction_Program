00400bde434a46cb1fea0b1aaef2466bd31a20df
Lucifer <raghv.afgji@icloud.com>
2025-01-10 13:34:19 +0530

    Code final changes done

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import art
 import os
+
 bid_dic = {}
 bid_condition = True
-highest_value = 0
-bid_winner = ''
+
 print(art.logo_auction) 
 print(art.logo_gavel)
+
+def highest_bidder(bid_dictionary):
+    highest_value = 0
+    for key in bid_dictionary:
+        if highest_value < bid_dic[key]:
+            highest_value = bid_dic[key]
+    return key,highest_value
+    
+    
+
 while bid_condition:
     name_key = input("Enter the name of the bidder.\n")
     bid_value = int(input("Enter the bid value in $\n"))
     bid_dic[name_key] = bid_value
+    bid_winner,winner_value = highest_bidder(bid_dic)
     print('\n'*100)
     condition_check = input("Enter 'Yes' if you want to continue bidding else enter 'No'\n").lower()
+    
     if condition_check != 'yes':
         bid_condition = False
 
-
-for key in bid_dic:
-    if highest_value < bid_dic[key]:
-        highest_value = bid_dic[key]
-        bid_winner = key
-
-print(f"Winner is {bid_winner} with bid amount {highest_value}.")
+print(f"Winner is {bid_winner} with bid amount {winner_value}.")
 os.system("pause")
