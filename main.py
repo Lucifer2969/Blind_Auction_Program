bda563efdf7a53b2ce47eef421d3b7a467d781c8
Lucifer <raghv.afgji@icloud.com>
2025-01-10 12:18:32 +0530

    Changes done

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ while bid_condition:
     name_key = input("Enter the name of the bidder.\n")
     bid_value = int(input("Enter the bid value in $\n"))
     bid_dic[name_key] = bid_value
-    print('\n'*100,end='')
+    print('\n'*100)
     condition_check = input("Enter 'Yes' if you want to continue bidding else enter 'No'\n").lower()
     if condition_check != 'yes':
         bid_condition = False
+
 
 for key in bid_dic:
     if highest_value < bid_dic[key]:
